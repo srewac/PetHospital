d792022539d8185c187d31db79653c9d81566ab0
srewac <srewac@126.com>
2018-03-08 13:14:40 +0800

    frontend and backend arc

--- PetHospital/settings.py
+++ PetHospital/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend.apps.FrontendConfig',
+    'backend.apps.BackendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
