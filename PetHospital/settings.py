1e2bddee828f072f366527e336881c8c86d02cd7
poi233 <593657039@qq.com>
2018-03-15 15:03:54 +0800

    fix server settings

--- PetHospital/settings.py
+++ PetHospital/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': '1234',
-        'HOST': ALLOWED_HOSTS,  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '127.0.0.1',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',  # Set to empty string for default.
     }
 }
