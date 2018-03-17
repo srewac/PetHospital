2ece142de5804fb3cc437cd6df01965288ccb023
poi233 <593657039@qq.com>
2018-03-17 16:05:59 +0800

    add backend architect draft

--- Manage/urls.py
+++ Manage/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from . import views
+from .views import dashboard, user, test, disease, basic
 
 app_name = 'Manage'
 urlpatterns = [
-    path('manage/', views.index, name='manage'),
-    path('forms/', views.forms, name='forms'),
+    path('', dashboard.index, name='index'),
+    path('user/', user.index, name='user_show'),
+    path('disease/', disease.index, name='disease_show'),
+    path('test/', test.index, name='test_show'),
 ]
