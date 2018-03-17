970881baccd3d13588bc66af2d9ab9e3a20ddde3
poi233 <593657039@qq.com>
2018-03-17 23:13:44 +0800

    add user example(part1)

--- Manage/urls.py
+++ Manage/urls.py
@@ -6,6 +6,7 @@ app_name = 'Manage'
 urlpatterns = [
     path('', dashboard.index, name='index'),
     path('user/', user.index, name='user_show'),
+    path('user/delete/<int:user_id>', user.user_delete, name='user_delete'),
     path('disease/', disease.index, name='disease_show'),
     path('test/test', test.test_index, name='test_show'),
     path('test/testpaper', test.testpapaer_index, name='testpaper_show')
--- Manage/views/user.py
+++ Manage/views/user.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from User.models import User
+from django.urls import reverse
 
 
 def index(request):
-    return render(request, 'backend/user/tables.html')
+    users = User.objects.all()
+    return render(request, 'backend/user/user_info.html', {'users': users})
+
+
+def user_delete(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    user.delete()
+    return redirect(reverse('Manage:user_show'))
+
