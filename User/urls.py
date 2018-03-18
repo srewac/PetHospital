62392842bda66a6c31e7babb5408cbd993ec3d91
SugarRay1 <petershao1996@outlook.com>
2018-03-18 18:16:22 +0800

    New Page of Sign in and Sign up. Add verify code. Fix several bugs in the process of sign in/up

--- User/urls.py
+++ User/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('sign_up/', views.sign_up, name='sign_up'),
     path('sign_in/', views.sign_in, name='sign_in'),
     path('sign_out/', views.sign_out, name='sign_out'),
+    path('test/', views.test, name='test'),
 ]
--- User/views.py
+++ User/views.py
@@ -12,40 +12,49 @@ def index(request):
 # 注册
 def sign_up(request):
     if request.method == "GET":
-        return render(request, 'User/index.html')
+        return render(request, 'User/sign_up.html')
     else:
         username = request.POST.get('username')
-        password1 = request.POST.get('password1')
-        email = request.POST.get('email')
+        email = request.POST.get('inputEmail')
+        password1 = request.POST.get('inputPassword')
         user1 = models.User.objects.filter(email=email)
         user2 = models.User.objects.filter(name=username)
         if user1:
-            return HttpResponse('该邮箱已经被注册，请更换邮箱')
+            return render(request, 'User/sign_up.html', {'register_err': "This e-mail has already been installed"})
         else:
             if user2:
-                return HttpResponse('该用户名已经被使用，请更换名称')
+                return render(request, 'User/sign_up.html', {'register_err': "This username has already been installed"})
             else:
                 models.User.objects.create(email=email, password=password1, name=username)
-                return render(request, 'User/index.html')
+                return render(request, 'User/sign_in.html')
 
 
 # 登陆
 def sign_in(request):
     if request.method == "GET":
-        return render(request, 'User/index.html')
+        return render(request, 'User/sign_in.html')
     else:
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = models.User.objects.get(email=email, password=password)
+        email = request.POST.get('inputEmail')
+        password = request.POST.get('inputPassword')
+        user = models.User.objects.filter(email=email, password=password).first()
+        # user = models.User.objects.get(email=email, password=password)
+
         if user:
             request.session['user_id'] = user.id
             request.session['username'] = user.name
             request.session['email'] = user.email
             request.session.set_expiry(600)
+            print(user.id)
             return HttpResponseRedirect('/User/index/')
+
         else:
-            return render(request, 'User/index.html', {'login_err': "用户名或密码错误"})
+            return render(request, 'User/sign_in.html',{'login_err': "Errors in Email or Password"})
+
 
 def sign_out(request):
     request.session.clear();
     return HttpResponseRedirect('/User/index/');
+
+
+def test(request):
+    return render(request, 'User/sign_in.html')
