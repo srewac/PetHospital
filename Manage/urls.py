1c6e8296608f6855c9f1592df0a0485070c1586c
poi233 <593657039@qq.com>
2018-03-18 12:39:00 +0800

    add model modify

--- Manage/urls.py
+++ Manage/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', dashboard.index, name='index'),
     path('user/', user.index, name='user_show'),
     path('user/delete/<int:user_id>', user.user_delete, name='user_delete'),
+    path('user/update/<int:user_id>', user.user_update, name='user_update'),
     path('disease/', disease.index, name='disease_show'),
     path('test/test', test.test_index, name='test_show'),
     path('test/testpaper', test.testpapaer_index, name='testpaper_show')
--- Manage/views/user.py
+++ Manage/views/user.py
@@ -12,4 +12,20 @@ def index(request):
 def user_delete(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     user.delete()
-    return JsonResponse([user_id], safe=False)
+    return redirect(reverse('Manage:user_show'))
+    # return JsonResponse([user_id], safe=False)
+
+
+def user_update(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    user.email = request.POST['email']
+    user.name = request.POST['name']
+    user.password = request.POST['password']
+    user.authority = request.POST['authority']
+    user.save()
+    user_dict = {'id': user.id,
+                 'email': user.email,
+                 'name': user.name,
+                 'password': user.password,
+                 'authority': user.authority}
+    return JsonResponse(user_dict)
