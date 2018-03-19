6970d9a804f12ad9d7bb752efafe9ce182c94bf0
srewac <srewac@126.com>
2018-03-19 11:35:11 +0800

    add email verify function on sign_up

--- User/views.py
+++ User/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib import auth
 from django.http import HttpResponse, HttpResponseRedirect
 from User import models
+from User import sendEmail
 
 
 # Create your views here.
@@ -17,16 +18,25 @@ def sign_up(request):
         username = request.POST.get('username')
         email = request.POST.get('inputEmail')
         password1 = request.POST.get('inputPassword')
-        user1 = models.User.objects.filter(email=email)
-        user2 = models.User.objects.filter(name=username)
-        if user1:
-            return render(request, 'User/sign_up.html', {'register_err': "This e-mail has already been installed"})
-        else:
-            if user2:
-                return render(request, 'User/sign_up.html', {'register_err': "This username has already been installed"})
+        verifyCode = request.POST.get('emailVerify')
+        if 'signup' in request.POST:
+            user1 = models.User.objects.filter(email=email)
+            user2 = models.User.objects.filter(name=username)
+            if user1:
+                return render(request, 'User/sign_up.html', {'register_err': "邮箱已经用于注册"})
+            elif user2:
+                return render(request, 'User/sign_up.html', {'register_err': "用户名已存在"})
+            elif verifyCode != request.session['verifyCode'] and request.session['verifyCode'] != None and request.session['username'] == username:
+                return render(request, 'User/sign_up.html', {'register_err': "邮箱验证码不正确"})
             else:
                 models.User.objects.create(email=email, password=password1, name=username)
                 return render(request, 'User/sign_in.html')
+        elif "emailVerify" in request.POST:
+            verifycode = sendEmail.random_str();
+            request.session['username'] = username;
+            request.session['verifyCode'] = verifycode;
+            sendEmail.send_register_email(email, verifycode, "register")
+            return render(request, 'User/sign_up.html',{})
 
 
 # 登陆
@@ -48,7 +58,7 @@ def sign_in(request):
             return HttpResponseRedirect('/User/index/')
 
         else:
-            return render(request, 'User/sign_in.html',{'login_err': "Errors in Email or Password"})
+            return render(request, 'User/sign_in.html',{'login_err': "用户名或密码错误"})
 
 
 def sign_out(request):
@@ -58,3 +68,4 @@ def sign_out(request):
 
 def test(request):
     return render(request, 'User/sign_in.html')
+
