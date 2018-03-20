3f63485c652ddfaf1a290c58d2b235795099f9d2
SugarRay1 <petershao1996@outlook.com>
2018-03-20 21:25:01 +0800

    Fix the bug: if session expire

--- User/views.py
+++ User/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth
 from django.http import HttpResponse, HttpResponseRedirect
 from User import models
 from User import sendEmail
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -19,24 +20,39 @@ def sign_up(request):
         email = request.POST.get('inputEmail')
         password1 = request.POST.get('inputPassword')
         verifyCode = request.POST.get('emailVerify')
-        if 'signup' in request.POST:
-            user1 = models.User.objects.filter(email=email)
-            user2 = models.User.objects.filter(name=username)
-            if user1:
-                return render(request, 'User/sign_up.html', {'register_err': "邮箱已经用于注册"})
-            elif user2:
-                return render(request, 'User/sign_up.html', {'register_err': "用户名已存在"})
-            elif verifyCode != request.session['verifyCode'] and request.session['verifyCode'] != None and request.session['username'] == username:
-                return render(request, 'User/sign_up.html', {'register_err': "邮箱验证码不正确"})
-            else:
-                models.User.objects.create(email=email, password=password1, name=username)
-                return render(request, 'User/sign_in.html')
-        elif "emailVerify" in request.POST:
-            verifycode = sendEmail.random_str();
-            request.session['username'] = username;
-            request.session['verifyCode'] = verifycode;
-            sendEmail.send_register_email(email, verifycode, "register")
-            return render(request, 'User/sign_up.html',{})
+
+        user1 = models.User.objects.filter(email=email)
+        user2 = models.User.objects.filter(name=username)
+        if user1:
+            return render(request, 'User/sign_up.html', {'register_err': "邮箱已经用于注册"})
+        elif user2:
+            return render(request, 'User/sign_up.html', {'register_err': "用户名已存在"})
+        elif 'verifyCode' not in request.session:
+            return render(request, 'User/sign_up.html', {'register_err': "邮箱验证码已过期"})
+        elif verifyCode != request.session['verifyCode'] and request.session['verifyCode'] != None and \
+                request.session['username'] == username:
+            return render(request, 'User/sign_up.html', {'register_err': "邮箱验证码不正确"})
+        else:
+            models.User.objects.create(email=email, password=password1, name=username)
+            return render(request, 'User/sign_in.html')
+
+
+
+
+def email_verify(request):
+    username = request.POST.get('username')
+    email = request.POST.get('inputEmail')
+    if not username :
+        return JsonResponse(0, safe=False)
+    elif not email:
+        return JsonResponse(1, safe=False)
+    else:
+        verifycode = sendEmail.random_str()
+        request.session['username'] = username
+        request.session['verifyCode'] = verifycode
+        request.session.set_expiry(300)
+        sendEmail.send_register_email(email, verifycode, "register")
+        return JsonResponse(2, safe=False)
 
 
 # 登陆
@@ -55,13 +71,13 @@ def sign_in(request):
             request.session['email'] = user.email
             request.session['authority'] = user.authority
             request.session.set_expiry(600)
-            if user.authority==1:
+            if user.authority == 1:
                 return HttpResponseRedirect('/Manage')
             else:
                 return HttpResponseRedirect('/User/index/')
 
         else:
-            return render(request, 'User/sign_in.html',{'login_err': "用户名或密码错误"})
+            return render(request, 'User/sign_in.html', {'login_err': "用户名或密码错误"})
 
 
 def sign_out(request):
@@ -71,4 +87,3 @@ def sign_out(request):
 
 def test(request):
     return render(request, 'User/sign_in.html')
-
