345a8466faa0ba88031bd8351537650a1e2a54e1
srewac <srewac@126.com>
2018-03-21 19:22:01 +0800

    [Test] module:select_disease

--- Test/urls.py
+++ Test/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('test/', views.test, name='test'),
-    path('select_paper/', views.select_paper, name='select_paper'),
+    path('select_disease/', views.select_disease, name='select_disease'),
+    path('<int:disease_id>/select_paper/', views.select_paper, name='select_paper'),
     path('test_management/', views.test_management, name='test_management'),
 ]
