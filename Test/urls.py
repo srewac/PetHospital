9396291ed36eafb3611ca8fb06f8e3098b605e68
srewac <srewac@126.com>
2018-03-22 10:45:47 +0800

    [Test] fix bug

--- Test/urls.py
+++ Test/urls.py
@@ -5,6 +5,6 @@ app_name = 'Test'
 urlpatterns = [
     path('test/', views.test, name='test'),
     path('select_disease/', views.select_disease, name='select_disease'),
-    path('<str:disease>/select_paper/', views.select_paper, name='select_paper'),
+    path('<str:disease_name>/select_paper/', views.select_paper, name='select_paper'),
     path('test_management/', views.test_management, name='test_management'),
 ]
