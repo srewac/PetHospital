d25c8c6f1c1c9390336893f48a46aebd9c387545
poi233 <593657039@qq.com>
2018-03-22 14:21:39 +0800

    refine code

--- Manage/views/user.py
+++ Manage/views/user.py
@@ -3,12 +3,12 @@ from User.models import User
 from django.urls import reverse
 from django.http import JsonResponse
 
-
+# 进入用户管理界面
 def index(request):
     users = User.objects.all()
     return render(request, 'backend/user/user_info.html', {'users': users})
 
-
+# 返回所有账户信息
 def user_dict(request):
     users = User.objects.all()
     user_d = {'data': []}
@@ -16,13 +16,13 @@ def user_dict(request):
         user_d['data'].append([user.email, user.name, user.password, '实习生' if user.authority == 0 else '管理员', user.id])
     return JsonResponse(user_d, safe=False)
 
-
+# 用户删除
 def user_delete(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     user.delete()
     return JsonResponse(True, safe=False)
 
-
+# 用户修改
 def user_update(request):
     user = get_object_or_404(User, pk=request.POST['id'])
     check_users = User.objects.filter(email=request.POST['email'])
@@ -37,7 +37,7 @@ def user_update(request):
     user.save()
     return JsonResponse(True, safe=False)
 
-
+# 用户创建
 def user_create(request):
     check_user = User.objects.filter(email=request.POST['email'])
     if len(check_user) > 0:
