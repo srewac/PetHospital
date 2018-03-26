44bcf6f2c033c6ed9298b0f3ffe21955c53a0cd7
SugarRay1 <petershao1996@outlook.com>
2018-03-26 19:39:19 +0800

    charge/medicine Manage

--- Manage/urls.py
+++ Manage/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 
 from .views import dashboard, user, test, disease
 
-from .views.basic import people,room
-
+from .views.basic import people, room, medicine, charge
 
 app_name = 'Manage'
 urlpatterns = [
@@ -30,6 +29,13 @@ urlpatterns = [
     path('test/question_modify_dict/<int:question_id>', test.question_modify_dict, name='question_modify_dict'),
     path('test/question_delete/<int:question_id>', test.question_delete, name='question_delete'),
 
+    # room urls
+    path('basic/room', room.room, name='room'),
+    path('basic/room_dict', room.room_dict, name='room_dict'),
+    path('basic/room_modify', room.room_modify, name='room_modify'),
+    path('basic/room_create', room.room_create, name='room_create'),
+    path('basic/room_delete/<int:room_id>', room.room_delete, name='room_delete'),
+
     # people urls
     path('basic/people', people.people, name="people_show"),
     path('basic/people/people_dict', people.people_dict, name='people_dict'),
@@ -37,11 +43,21 @@ urlpatterns = [
     path('basic/people/update', people.people_update, name='people_update'),
     path('basic/people/create', people.people_create, name='people_create'),
 
-    # room urls
-    path('basic/room', room.room, name='room'),
-    path('basic/room_dict', room.room_dict, name='room_dict'),
-    path('basic/room_modify', room.room_modify, name='room_modify'),
-    path('basic/room_create', room.room_create, name='room_create'),
-    path('basic/room_delete/<int:room_id>', room.room_delete, name='room_delete'),
+    # medicine urls
+    path('basic/medicine', medicine.medicine, name="medicine_show"),
+    path('basic/medicine/medicine_dict', medicine.medicine_dict, name='medicine_dict'),
+    path('basic/medicine/delete/<int:medicine_id>', medicine.medicine_delete, name='medicine_delete'),
+    path('basic/medicine/update', medicine.medicine_update, name='medicine_update'),
+    path('basic/medicine/create', medicine.medicine_create, name='medicine_create'),
+
+    # charge urls
+    path('basic/charge', charge.charge, name="charge_show"),
+    path('basic/charge/charge_dict', charge.charge_dict, name='charge_dict'),
+    path('basic/charge/delete/<int:charge_id>', charge.charge_delete, name='charge_delete'),
+    path('basic/charge/modify_charge_init/<int:people_id>', charge.modify_charge_init, name='modify_charge_init'),
+    path('basic/charge/modify_charge_room_select', charge.modify_charge_room_select, name='modify_charge_room_select'),
+    path('basic/charge/update', charge.charge_update, name='charge_update'),
+    path('basic/charge/create_charge_room_select', charge.create_charge_room_select, name='create_charge_room_select'),
+    path('basic/charge/create', charge.charge_create, name='charge_create'),
 
 ]
