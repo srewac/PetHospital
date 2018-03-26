f18edd21c0c07c92e27f6ecc92f75337a878955c
srewac <srewac@126.com>
2018-03-26 19:38:24 +0800

    [Test] module:test & fix typo

--- Test/views.py
+++ Test/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import HttpResponseRedirect,Http404,render_to_response
-from django.template import loader
-from Test.models import TestPaper
+from Test.models import *
 from Disease.models import Disease
-
 
 
 # Create your views here.
@@ -11,20 +9,18 @@ def test(request):
     if request.session.get('username', None):
         return render(request, 'Test/test.html')
     else:
-        return HttpResponseRedirect('/User/sign_in/');
+        return HttpResponseRedirect('/User/sign_in/')
 
 def select_paper(request, disease_name):
     if request.session.get('username', None):
         if request.method == "GET":
             disease = Disease.objects.get(name=disease_name)
-            testpaper = TestPaper.objects.filter(disease=disease)
+            #testpaper = disease.testpaper_set.all()
+            test = Test.objects.filter(test_paper__disease__name=disease_name)
+            #testpaper = TestPaper.objects.filter(disease=disease)
             return render_to_response('Test/select_paper.html', locals())
-        else:
-            paper = request.POST.get('selectedpaper')
-            # TODO: page for single test
-            return render(request, 'User/index.html')
     else:
-        return HttpResponseRedirect('/User/sign_in/');
+        return HttpResponseRedirect('/User/sign_in/')
 
 
 def select_disease(request):
@@ -34,7 +30,23 @@ def select_disease(request):
             disease = Disease.objects.all()
             return render_to_response('Test/select_disease.html', locals())
     else:
-        return HttpResponseRedirect('/User/sign_in/');
+        return HttpResponseRedirect('/User/sign_in/')
 
-def test_management(request):
-    return render(request, 'Test/test_management.html')
+def selected_paper(request):
+    test_id = request.POST.get('test_id')
+    request.session['test_id'] = test_id
+    return render(request, 'Test/test.html')
+
+def paper(request):
+    if request.session.get('test_id', None):
+        if request.method =="GET":
+            test_id = request.session['test_id']
+            selectedtest = Test.objects.get(id=test_id)
+            choice = Choice.objects.filter(question__testpaper__test=test_id)
+            return render_to_response('Test/paper.html', locals())
+        else:
+            del request.session['test_id']
+            #TODO: submit answers
+            return HttpResponseRedirect('/Test/test/')
+    else:
+        return HttpResponseRedirect('/Test/test/')
