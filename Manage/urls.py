b4d3e30112924ec097d8697df6db8a24295ab352
szone <1160376296@qq.com>
2018-03-26 23:43:52 +0800

    Add analysis inhospital and vaccine, should to re-migrate to run

--- Manage/urls.py
+++ Manage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import dashboard, user, test, disease
 
-from .views.basic import people，room
+from .views.basic import people, room, analysis, vaccine, inhospital
 
 
 app_name = 'Manage'
@@ -38,10 +38,31 @@ urlpatterns = [
     path('basic/people/create', people.people_create, name='people_create'),
 
     # room urls
-    path('basic/room', room.room, name='room'),
+    path('basic/room', room.room, name='room_show'),
     path('basic/room_dict', room.room_dict, name='room_dict'),
     path('basic/room_modify', room.room_modify, name='room_modify'),
     path('basic/room_create', room.room_create, name='room_create'),
     path('basic/room_delete/<int:room_id>', room.room_delete, name='room_delete'),
 
+    # analysis urls
+    path('basic/analysis', analysis.analysis, name='analysis_show'),
+    path('basic/analysis_dict', analysis.analysis_dict, name='analysis_dict'),
+    path('basic/analysis_create', analysis.analysis_create, name='analysis_create'),
+    path('basic/analysis_delete/<int:analysis_id>', analysis.analysis_delete, name='analysis_delete'),
+
+    # vaccine urls
+    path('basic/vaccine', vaccine.vaccine, name='vaccine_show'),
+    path('basic/vaccine_dict', vaccine.vaccine_dict, name='vaccine_dict'),
+    path('basic/vaccine_modify', vaccine.vaccine_modify, name='vaccine_modify'),
+    path('basic/vaccine_create', vaccine.vaccine_create, name='vaccine_create'),
+    path('basic/vaccine_delete/<int:vaccine_id>', vaccine.vaccine_delete, name='vaccine_delete'),
+
+    # inhospital urls
+    path('basic/inhospital', inhospital.inhospital, name='inhospital_show'),
+    path('basic/inhospital_dict', inhospital.inhospital_dict, name='inhospital_dict'),
+    path('basic/inhospital_modify_dict/<int:id>', inhospital.inhospital_modify_dict, name='inhospital_modify_dict'),
+    path('basic/inhospital_modify', inhospital.inhospital_modify, name='inhospital_modify'),
+    path('basic/inhospital_create_dict', inhospital.inhospital_create_dict, name='inhospital_create_dict'),
+    path('basic/inhospital_create', inhospital.inhospital_create, name='inhospital_create'),
+    path('basic/inhospital_delete/<int:patient_id>', inhospital.inhospital_delete, name='inhospital_delete'),
 ]
