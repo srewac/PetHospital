a1b286f61faa6d9851e14b5eb2c918b6c58063f1
srewac <srewac@126.com>
2018-03-28 11:13:12 +0800

    [Test] test:calculate score and save into database

--- Test/views.py
+++ Test/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import HttpResponseRedirect,Http404,render_to_response
 from Test.models import *
+from User.models import *
 from Disease.models import Disease
 
 
@@ -38,21 +39,31 @@ def selected_paper(request):
     return render(request, 'Test/test.html')
 
 def paper(request):
-    if request.session.get('test_id', None):
-        test_id = request.session['test_id']
-        selectedtest = Test.objects.get(id=test_id)
-        question = Question.objects.filter(testpaper__test=test_id)
-        choice = Choice.objects.filter(question__testpaper__test=test_id)
-        if request.method =="GET":
-            return render_to_response('Test/paper.html', locals())
+    if request.session.get('username', None):
+        if request.session.get('test_id', None):
+            test_id = request.session['test_id']
+            user_name = request.session['username']
+            selectedtest = Test.objects.get(id=test_id)
+            question = Question.objects.filter(testpaper__test=test_id)
+            choice = Choice.objects.filter(question__testpaper__test=test_id)
+            if request.method =="GET":
+                return render_to_response('Test/paper.html', locals())
+            else:
+                user_id = User.objects.get(name= user_name).id
+                score = 0
+                for question_id in question:
+                    id = question_id.id
+                    answer = request.POST.get(str(id))
+                    c_answer = Choice.objects.get(id=answer)
+                    c_score = Question.objects.get(id=id).score
+                    if(c_answer.correct==True):
+                        score += c_score
+                    c_score = 0
+                user_testscore = Usertest(user_id=user_id,test_id=test_id,score=score)
+                user_testscore.save()
+                del request.session['test_id']
+                return HttpResponseRedirect('/Test/test/')
         else:
-            #TODO: submit answers
-            for question_id in question:
-                id = question_id.id
-                answer = request.POST.get(str(id))
-                print(id)
-                print(answer)
-            del request.session['test_id']
             return HttpResponseRedirect('/Test/test/')
     else:
-        return HttpResponseRedirect('/Test/test/')
+        return HttpResponseRedirect(request,'/User/sign_in')
