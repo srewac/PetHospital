e554b751b7f1b52c5719b70ca21c5ef9f6c297f6
poi233 <593657039@qq.com>
2018-03-31 19:07:31 +0800

    add compulsive login for backend

--- PetHospital/views.py
+++ PetHospital/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from User import models
 from User import sendEmail
 from django.http import JsonResponse
@@ -9,7 +10,10 @@ from django.http import JsonResponse
 # Create your views here.
 def index(request):
     if request.session.get('username', None):
-        return render(request, 'index.html')
+        if request.session.get('authority', None) == 0:
+            return render(request, 'index.html')
+        else:
+            return redirect(reverse('Manage:index', args=[]))
     else:
         return render(request, 'User/sign_in.html')
 
--- Test/urls.py
+++ Test/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
 from . import views
+
 app_name = 'Test'
 urlpatterns = [
     path('test/', views.test, name='test'),
