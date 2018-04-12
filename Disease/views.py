f4bca055f1247718d01311b8c19705560ac72d4a
SugarRay1 <petershao1996@outlook.com>
2018-04-12 14:39:11 +0800

    Link of subdisease and disease_example

--- Disease/views.py
+++ Disease/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponseRedirect,Http404,render_to_response
+from django.shortcuts import HttpResponseRedirect, Http404, render_to_response
 from Test.models import TestPaper
 from Disease.models import Disease
 from Disease.models import SubDisease
+from Disease.models import DiseaseExample
+
 
 def select_disease(request):
     if request.session.get('username', None):
@@ -24,10 +26,20 @@ def select_subdisease(request, disease_name):
         return HttpResponseRedirect('/User/sign_in/')
 
 
-def subdisease_desc(request,subdisease_id):
+def subdisease_desc(request, subdisease_id):
     if request.session.get('username', None):
         if request.method == "GET":
-            subdisease=SubDisease.objects.get(id=subdisease_id)
+            subdisease = SubDisease.objects.get(id=subdisease_id)
+            disease_examples = DiseaseExample.objects.filter(sub_disease=subdisease)
             return render_to_response('Disease/subdisease_desc.html', locals())
+    else:
+        return HttpResponseRedirect('/User/sign_in/')
+
+
+def disease_example_desc(request,disease_example_id):
+    if request.session.get('username', None):
+        if request.method == "GET":
+            disease_example = DiseaseExample.objects.get(id=disease_example_id)
+            return render_to_response('Disease/disease_example_desc.html', locals())
     else:
         return HttpResponseRedirect('/User/sign_in/')
