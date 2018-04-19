27033b8fcfeb5e87a04ab3aa00b4d1a57a2afec4
poi233 <593657039@qq.com>
2018-04-19 13:11:26 +0800

    set Test environment

--- Navigation/views.py
+++ Navigation/views.py
@@ -1,16 +1,10 @@
-from django.shortcuts import render,render_to_response,HttpResponseRedirect
+from django.shortcuts import render, render_to_response, HttpResponseRedirect
+
 
 # Create your views here.
 def hall(request):
-    if request.session.get('username', None):
-        return render(request, 'Navigation/hall.html')
-    else:
-        return HttpResponseRedirect('/User/sign_in/')
+    return render(request, 'Navigation/hall.html')
 
 
 def ward(request):
-    if request.session.get('username', None):
-        return render(request, 'Navigation/ward.html')
-
-    else:
-        return HttpResponseRedirect('/User/sign_in/')
+    return render(request, 'Navigation/ward.html')
